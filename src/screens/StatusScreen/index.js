c81bf5b88e13aff2b49c9c3ade0a96a95a74642d
Jason Constantine <jconstantine@ycp.edu>
2019-09-11 16:07:31 -0400

    Basic Header and Status light implimented

--- src/screens/StatusScreen/index.js
+++ src/screens/StatusScreen/index.js
@@ -1,17 +1,32 @@
-import {Image, Text, TouchableHighlight, View} from 'react-native';
+import {Image, Text, TouchableHighlight, View,Animate} from 'react-native';
 import React from 'react';
 import styles from './styles';
+
+var statuslightstyle = styles.statuslight;
 
 class StatusScreen extends React.Component {
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Status Screen</Text>
+        <Text style= {styles.header}>Status</Text>
+
+        <View style={statuslightstyle} />
+        <Text style= {styles.sensorlistheader}>Sensors</Text>
+        {/* Start Sensor List */}
+        <View style={styles.sensorlistcontainer}>
+            <TouchableHighlight onPress={() => {this.props.navigation.navigate('Sensor')}}>
+                <Text>Test1</Text>
+            </TouchableHighlight>
+            <Text>Test</Text>
+            <Text>Test2</Text>
+            <Text>Test3</Text>
+        </View>
+        {/*Back Arrow*/}
         <TouchableHighlight onPress={() => this.props.navigation.goBack()} style={styles.back}>
-          <Image
-            source={require("../../../assets/images/back.png")}
-          />
+         <Image
+           source={require("../../../assets/images/back.png")}
+         />
         </TouchableHighlight>
       </View>
     );
--- src/screens/StatusScreen/styles.js
+++ src/screens/StatusScreen/styles.js
@@ -1,10 +1,40 @@
 import {StyleSheet} from 'react-native';
 
+var statuslightsize = 50;
+
 export default StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  header:{
+    position: 'absolute',
+    top: '10%',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+
+  statuslight: {
+  width: 50,
+  height: 50,
+  backgroundColor: 'green',
+  borderWidth: 10,
+  borderColor: 'transparent',
+  borderRadius: 30,
+  top: '9.5%',
+  right: '25%',
+  position: 'absolute',
+  },
+  sensorlistheader:{
+    position: 'absolute',
+    top: '20%',
+  },
+  sensorlistcontainer: {
+    position: 'absolute',
+    top: '25%',
+    flex: 1,
+    flexDirection: 'column'
   },
   back:{
     position: 'absolute',
