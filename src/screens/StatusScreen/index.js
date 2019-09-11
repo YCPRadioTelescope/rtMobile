9ede86f6b8563e4926da24fc05afe95ffee2900c
asfopoo <enardo@ycp.edu>
2019-09-11 17:18:56 -0400

    fix master

--- src/screens/StatusScreen/index.js
+++ src/screens/StatusScreen/index.js
@@ -6,43 +6,14 @@ import styles from './styles';
 
 class StatusScreen extends React.Component {
 
-constructor() {
-   super();
- }
-
-let statuslightstyle {};
-
-swapstatuscolor () {
-    console.log('Yellow Pressed');
-    statuslightstyle = {};
-}
-
   render() {
     return (
       <View style={styles.container}>
-        <Text style= {styles.header}>Status</Text>
-
-        <View style={statuslightstyle} />
-        <Text style= {styles.sensorlistheader}>Sensors</Text>
-        <View style={styles.sensorlistcontainer}>
-            <TouchableHighlight onPress={() => {this.props.navigation.navigate('Sensor')}}>
-                <Text>Test1</Text>
-            </TouchableHighlight>
-            <TouchableHighlight onPress={() => {this.props.navigation.navigate('Sensor')}}>
-                <Text>Test2</Text>
-            </TouchableHighlight>
-            <TouchableHighlight onPress={() => {this.props.navigation.navigate('Sensor')}}>
-                <Text>Test3</Text>
-            </TouchableHighlight>
-            <TouchableHighlight onPress={this.swapstatuscolor}>
-                <Text>Yellow</Text>
-            </TouchableHighlight>
-        </View>
-        {/*Back Arrow*/}
+        <Text>Status Screen</Text>
         <TouchableHighlight onPress={() => this.props.navigation.goBack()} style={styles.back}>
-         <Image
-           source={require("../../../assets/images/back.png")}
-         />
+          <Image
+            source={require("../../../assets/images/back.png")}
+          />
         </TouchableHighlight>
       </View>
     );
