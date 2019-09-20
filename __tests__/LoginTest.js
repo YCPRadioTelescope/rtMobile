469a2dcbab7e44f4bcd78218eeb24a025fa77a20
asfopoo <enardo@ycp.edu>
2019-09-20 03:23:45 -0400

    testing

--- __tests__/LoginTest.js
+++ __tests__/LoginTest.js
@@ -1,6 +1,24 @@
 import mockStore from 'redux-mock-store';
 import {login} from '../src/screens/LoginScreen/AuthActions';
 const store = mockStore() ;
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+
+// This sets the mock adapter on the default instance
+const mock = new MockAdapter(axios);
+
+// Mock any GET request to /users
+// arguments for reply are (status, data, headers)
+mock.onPost('/login').reply(200, {
+  users: [
+    { id: 1, name: 'John Smith' }
+  ]
+});
+
+axios.get('/users')
+  .then(function(response) {
+    console.log(response.data);
+  });
 
 describe('Testing log in authentication', () => {
   beforeEach(() => {
