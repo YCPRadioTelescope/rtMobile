3c520a54c6813d525be17a684c88fe8b7105226b
unknown <amcdevitt97@gmail.com>
2019-09-30 16:55:31 -0400

    Adding email functionality

--- src/components/rectangleButton/RectangleButton.js
+++ src/components/rectangleButton/RectangleButton.js
@@ -9,30 +9,46 @@ class RectangleButton extends Component {
       userID: this.props.userID,
     };
   }
-  
-  sendEmail(email, subject, body){
-    console.log('Sending denial email');
-    console.log('To:'+email);
-    console.log('Subject:'+subject);
-    console.log('Body: '+body);
+  // See: https://github.com/sendgrid/sendgrid-nodejs/issues/222
+  sendEmail(to, subject, body){
+    let content = {
+        "personalizations":[ {
+            "to": [ { "email": to } ],
+            "subject": subject } ],
+            "from": { "email": "from_address@example.com" },
+            "content": [ { "type": "text/plain",
+            "value": body
+        } ]
+    }
+
+     fetch('https://api.sendgrid.com/v3/mail/send', {
+       method: 'POST',
+       headers: {
+     	'Authorization': 'Bearer ' + process.env.SENDGRID_API_KEY,
+     	'Content-Type': 'application/json',
+       },
+       body: JSON.stringify(content),
+       }).then((response) => {
+            this.setState({response: `${response.status} - ${response.ok}`});
+       });
   }
 
-  onPress = () => {
-    
-    var ID = this.props.userID;
-    // TODO: get email from userID
-    var email = 'amcdevitt97@gmail.com';
-    // TODO: get name from userID
-    var name = 'Alyssa McDevitt';
-    var subject = 'Needs review: Your YCAS radio telescope account was denied';
-    var body = "Dear "+name+",  \nYour account for the YCAS radio telescope has"
-    +" been denied for the following reason: " + this.props.reason +". If you "
-    +"think this was in error, please email important people at an important address.";
-    this.sendEmail(email, subject, body);
-    this.props.navigation.goBack();
+  onPress = () => {
+    var ID = this.props.userID;
+    // TODO: get email from userID
+    var email = 'amcdevitt97@gmail.com'
+    // TODO: get name from userID
+    var name = 'Alyssa McDevitt';
+    var subject = 'Needs review: Your YCAS radio telescope account was denied';
+    var body = "Dear "+name+",  \nYour account for the YCAS radio telescope has"
+    +" been denied for the following reason: " + this.props.reason +". If you "
+    +"think this was in error, please email important people at an important address.";
+    this.sendEmail(email, subject, body);
+    this.props.navigation.goBack();
   }
 
  render() {
+    console.log(this.state);
     return (
         <View>
             <TouchableHighlight style={styles.button} onPress={this.onPress}>
