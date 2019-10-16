7f8484965edcb567f8483ea1e5d718c0a7e5427c
Alyssa McDevitt <amcdevitt97@gmail.com>
2019-10-16 16:25:49 -0400

    Added home screen nav bar and template footage

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,19 +1,32 @@
 import {Image, Text, TouchableHighlight, View} from 'react-native';
 import React from 'react';
 import styles from './styles';
+import ScrollElements from "../../components/scrollView/scrollView";
 
 class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Home Screen</Text>
-        <TouchableHighlight onPress={() => this.props.navigation.goBack()} style={styles.back}>
-          <Image
-            source={require("../../../assets/images/back.png")}
-          />
-        </TouchableHighlight>
-      </View>
+        <View style={styles.container}>
+            {/*      BEGIN NAVBAR           */}
+            <View style={styles.navBar}>
+                <Text style={styles.navTitle}>Status: </Text>
+                <Image
+                    source={require("../../../assets/images/redStatus.png")}
+                    style={styles.mainStatusLight}/>
+            </View>
+            <TouchableHighlight onPress={() => this.props.navigation.goBack()} style={styles.back}>
+                <Image
+                    source={require("../../../assets/images/back.png")}
+                />
+            </TouchableHighlight>
+            {/*             END NAVBAR            */}
+            {/*             BEGIN FOOTAGE         */}
+            <Image
+                source={require("../../../assets/images/footage.jpg")}  style={styles.footageScreen}/>
+            {/*             END FOOTAGE            */}
+
+        </View>
     );
   }
 }
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -3,12 +3,45 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection:'column',
+    backgroundColor: '#f5f5f5',
+  },
+  navBar: {
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: '#cebaff',
+    height:'12%',
+    marginLeft:0,
+    marginRight:0,
+  },
+  navTitle:{
+    position: 'absolute',
+    top:'30%',
+    left:'35%',
+    fontSize:25,
+    fontWeight:'bold',
   },
   back:{
     position: 'absolute',
     left: '10%',
-    top: '10%',
-  }
+    top: '5%',
+  },
+  mainStatusLight:{
+    resizeMode:'contain',
+    width:'7%',
+    height:'37%',
+    borderRadius: 100,
+    top:'2%',
+    left:'15%',
+  },
+  footageScreen:{
+    position: 'absolute',
+    resizeMode:'cover',
+    width:'100%',
+    height:'45%',
+    top:'12%',
+    borderColor:'#000',
+    borderWidth:5,
+  },
+
 });
