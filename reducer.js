05c71c326ec0e8e0c167d2c1d2a57b1e917c84c8
Alyssa McDevitt <amcdevitt97@gmail.com>
2019-10-30 11:01:35 -0400

    Updated reducers in the reducer.js file to include our new reducers

--- reducer.js
+++ reducer.js
@@ -1,13 +1,20 @@
 import { combineReducers } from "redux";
-import AuthReducer from "./src/screens/LoginScreen/AuthReducer";
 import emailReducer from "./src/reducers/emailReducer";
 import getUsersReducer from "./src/reducers/getUsersReducer";
 import sensorReducer from "./src/screens/SensorScreen/SensorReducer";
+import denyUserReducer from "./src/reducers/denyUserReducer";
+import approveUserReducer from "./src/reducers/approveUserReducer";
+import createInactiveUserReducer from "./src/reducers/createInactiveUserReducer";
+import deleteUserReducer from "./src/reducers/deleteUserReducer";
 
 const reducer = combineReducers({
   //user: AuthReducer,
   email: emailReducer,
   user: getUsersReducer,
+  denyUser: denyUserReducer,
+  approveUser: approveUserReducer,
+  createInactiveUser: createInactiveUserReducer,
+  deleteUser: deleteUserReducer,
   sensor: sensorReducer,
 });
 
