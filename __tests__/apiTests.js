9c68cb0294402f559cc84c7707386e20549388d5
asfopoo <enardo@ycp.edu>
2019-11-11 21:13:32 -0500

    api testing for non 200 responses

--- __tests__/apiTests.js
+++ __tests__/apiTests.js
@@ -68,6 +68,94 @@ describe("correct api calls", () => {
     const response = await fetch('http://rtWebService.us-east-1.elasticbeanstalk.com/api-docs');
     expect(response.status).toBe(200);
   });
+});
 
+describe("bad credentials api calls", () => {
+  it('pendingUsers 403', async () => {
 
+    const response = await fetch('http://rtWebService.us-east-1.elasticbeanstalk.com/pendingUsers', {
+      method: "POST",
+      body: JSON.stringify({
+        "UUID": "badString",
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    expect(response.status).toBe(403);
+  });
+
+  it('users 403', async () => {
+
+    const response = await fetch('http://rtWebService.us-east-1.elasticbeanstalk.com/users', {
+      method: "POST",
+      body: JSON.stringify({
+        "UUID": "badString",
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    expect(response.status).toBe(403);
+  });
+
+  it('approveUser 403', async () => {
+
+    const response = await fetch('http://rtWebService.us-east-1.elasticbeanstalk.com/approveUser', {
+      method: "POST",
+      body: JSON.stringify({
+        "UUID": "badString",
+        id: 1
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    expect(response.status).toBe(403);
+  });
+
+  it('denyUser 403', async () => {
+
+    const response = await fetch('http://rtWebService.us-east-1.elasticbeanstalk.com/denyUser', {
+      method: "POST",
+      body: JSON.stringify({
+        "UUID": "badString",
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        id: 1
+      }
+    });
+    expect(response.status).toBe(403);
+  });
+});
+
+describe("api calls bad params", () => {
+  it('approveUser 401', async () => {
+
+    const response = await fetch('http://rtWebService.us-east-1.elasticbeanstalk.com/approveUser', {
+      method: "POST",
+      body: JSON.stringify({
+        "UUID": config.UUID,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    expect(response.status).toBe(401);
+  });
+
+  it('denyUser 401', async () => {
+
+    const response = await fetch('http://rtWebService.us-east-1.elasticbeanstalk.com/denyUser', {
+      method: "POST",
+      body: JSON.stringify({
+        "UUID": config.UUID,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    expect(response.status).toBe(401);
+  });
 });
