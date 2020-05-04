7be2bd4b4e8a3eb9199cb02644861a219373fd8a
Jason Constantine <jconstantine@ycp.edu>
2020-05-03 20:24:06 -0400

    Accidentaly overrode my changes to sensor screen with the version on cleanup branch. This has been fixed

--- src/screens/SensorScreen/index.js
+++ src/screens/SensorScreen/index.js
@@ -7,7 +7,8 @@ import {bindActionCreators} from "redux";
 import {email} from "../../actions/emailAction";
 import {approveUser} from "../../actions/approveUserAction";
 import connect from "react-redux/lib/connect/connect";
-
+import { LineChart,XAxis, YAxis, Grid } from 'react-native-svg-charts'
+import * as shape from 'd3-shape'
 
 const Detail = ({
                     key,
@@ -34,16 +35,27 @@ const Detail = ({
 class SensorScreen extends React.Component {
 
 
-        state = {
-            //this is the array that holds information the the sensor components
-            buttonText: "Activate Override",
-            //azimuth: this.props.navigation.getParam("azimuth", 45),
-            sensorName: this.props.navigation.getParam('sensorname', 'Sensor'),
-            status:  this.props.navigation.getParam('status', 3),
-            override: this.props.navigation.getParam('override',0),
-            id: this.props.navigation.getParam('id',-1),
-            sensor: this.props.navigation.getParam('sensor')
-        }
+    state = {
+        //this is the array that holds information the the sensor components
+        buttonText: "Activate Override",
+        //azimuth: this.props.navigation.getParam("azimuth", 45),
+        sensorName: this.props.navigation.getParam('sensorname', 'Sensor'),
+        status:  this.props.navigation.getParam('status', 3),
+        override: this.props.navigation.getParam('override',0),
+        id: this.props.navigation.getParam('id',-1),
+        sensor: this.props.navigation.getParam('sensor'),
+        initdata : [50, 10, 40, 95, 20, 0, 85, 91, 35, 53, 6, 24, 50, 10, 0,70,65,62,58,55,55,50,40,35,58,55,55,50,40,35,
+            50, 10, 40, 95, 20, 0, 85, 91, 35, 53, 6, 24, 50, 10, 0,70,65,62,58,55,55,50,40,35,58,55,55,50,40,35,
+            50, 10, 40, 95, 20, 0, 85, 91, 35, 53, 6, 24, 50, 10, 0,70,65,62,58,55,55,50,40,35,58,55,55,50,40,35
+        ],
+        data : [50, 10, 40, 95, 20, 0, 85, 91, 35, 53, 6, 24, 50, 10, 0,70,65,62,58,55,55,50,40,35,58,55,55,50,40,35,
+            50, 10, 40, 95, 20, 0, 85, 91, 35, 53, 6, 24, 50, 10, 0,70,65,62,58,55,55,50,40,35,58,55,55,50,40,35,
+            50, 10, 40, 95, 20, 0, 85, 91, 35, 53, 6, 24, 50, 10, 0,70,65,62,58,55,55,50,40,35,58,55,55,50,40,35
+        ],
+        verticalContentInset : {  top: 10, bottom: 10 },
+        axesSvg : { fontSize: 10, fill: 'grey' },
+        xAxisHeight : 30,
+    }
 
 
     getLightColor = (status,override) =>{
@@ -76,13 +88,13 @@ class SensorScreen extends React.Component {
 
     updateOverride = () =>{
         if(this.state.override){
-            //console.log("Turning off override at sensor ",this.state.id);
+            console.log("Turning off override at sensor ",this.state.id);
             this.setState({buttonText: "Activate Override"});
             this.setState({override: 0})
             this.props.setOverride(this.state.sensor.name,0);
         }
         else{
-            //console.log("Turning oon override at sensor ",this.state.id);
+            console.log("Turning on override at sensor ",this.state.id);
             this.setState({buttonText: "Remove Override"});
             this.setState({override: 1})
             this.props.setOverride(this.state.sensor.name,1);
@@ -90,42 +102,139 @@ class SensorScreen extends React.Component {
 
     };
 
-  render() {
-    const { navigation } = this.props;
-    const status = navigation.getParam('details', 3)
-      //console.log("This sensor's id is: ",this.state.id);
-      //console.log("The state.sensor in sensor screen",this.state.sensor);
-    return (
-        <ScrollView>
-            <TouchableHighlight onPress={() => this.props.navigation.goBack()} style={styles.back}>
-                <Image
-                    source={require("../../../assets/images/back.png")}
-                />
-            </TouchableHighlight>
-            <View style={{marginTop: '10%', alignItems: 'center',}}>
-                <Text style = {styles.header}> {this.state.sensorName}</Text>
+    updateChart = (option) => {
+        /*
+        Have one big array that serves as a base. Then chop up based on option provided
+         */
+        if(option == 0){//1 day
+            //alert("Button Number 0 Pressed!")
+            this.setState({
+                data: this.state.initdata.slice(0,24)
+            });
+        }
+        else if(option == 1){//1 week
+            console.log("option number 1 pressed")
+            this.setState({
+                data: this.state.initdata.slice(0,31)
+            });
+        }
+        else if(option == 2){//1 month
+            console.log("option number 2 pressed")
+            this.setState({
+                data: this.state.initdata.slice(0,31)
+            });
+        }
+        else if (option == 3){// 6 months
+            console.log("option number 3 pressed")
+            this.setState({
+                data: this.state.initdata
+            });
+        }
+        else{
+            alert("Error, Invalid option");
+        }
 
-            </View>
-            <Divider style={styles.sectionDivider}/>
-            <View style={styles.container}>
-                <View style={styles.detailslistcontainer}>
-                    <TouchableHighlight onPress={() => {}}>
-                        <Detail name = {this.state.sensorName} status={this.state.status}
-                        style = {styles.statusLightStyle}
-                        image = {this.getLightColor(this.state.status,this.state.override)}
+        //alert("Button Number "+option+" Pressed!")
+    };
+
+    render() {
+        const { navigation } = this.props;
+
+
+
+
+        const status = navigation.getParam('details', 3)
+        //console.log("This sensor's id is: ",this.state.id);
+        console.log("The state.sensor in sensor screen",this.state.sensor);
+        return (
+            <ScrollView>
+                <TouchableHighlight onPress={() => this.props.navigation.goBack()} style={styles.back}>
+                    <Image
+                        source={require("../../../assets/images/back.png")}
+                    />
+                </TouchableHighlight>
+                <View style={{marginTop: '10%', alignItems: 'center',}}>
+                    <Text style = {styles.header}> {this.state.sensorName}</Text>
+
+                </View>
+                <Divider style={styles.sectionDivider}/>
+                <View style={styles.container}>
+                    <View style={styles.detailslistcontainer}>
+                        <TouchableHighlight onPress={() => {}}>
+                            <Detail name = {this.state.sensorName} status={this.state.status}
+                                    style = {styles.statusLightStyle}
+                                    image = {this.getLightColor(this.state.status,this.state.override)}
+                            />
+                        </TouchableHighlight>
+                    </View>
+                </View>
+                <TouchableHighlight onPress={this.updateOverride} style={styles.button}>
+                    <View>
+                        <Text style={{color: 'white'}}> {this.state.buttonText} </Text>
+                    </View>
+                </TouchableHighlight>
+
+                <View style={styles.chart}>
+                    <YAxis
+                        data={this.state.data}
+                        style={{ marginBottom: this.state.xAxisHeight }}
+                        contentInset={this.state.verticalContentInset}
+                        svg={this.state.axesSvg}
+                    />
+                    <View style={{ flex: 1, marginLeft: 10 }}>
+                        <LineChart
+                            style={{ flex: 1 }}
+                            data={this.state.data}
+                            contentInset={this.state.verticalContentInset}
+                            svg={{ stroke: 'rgb(134, 65, 244)' }}
+                        >
+                            <Grid/>
+                        </LineChart>
+                        <XAxis
+                            style={{ marginHorizontal: -10, height: this.state.xAxisHeight }}
+                            data={this.state.data}
+                            formatLabel={(value, index) => index}
+                            contentInset={{ left: 10, right: 10 }}
+                            svg={this.state.axesSvg}
                         />
+                    </View>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                    <TouchableHighlight onPress={() =>this.updateChart(0)} style={styles.histbutton}>
+                        <View>
+                            <Text style={{color: 'white'}}> 1 day </Text>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableHighlight onPress={() =>this.updateChart(1)} style={styles.histbutton}>
+                        <View>
+                            <Text style={{color: 'white'}}> 1 Week </Text>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableHighlight onPress={() =>this.updateChart(2)} style={styles.histbutton}>
+                        <View>
+                            <Text style={{color: 'white'}}> 1 Month </Text>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableHighlight onPress={() =>this.updateChart(3)} style={styles.histbutton}>
+                        <View>
+                            <Text style={{color: 'white'}}> 3 Months </Text>
+                        </View>
                     </TouchableHighlight>
                 </View>
-            </View>
-            <TouchableHighlight onPress={this.updateOverride} style={styles.button}>
-                <View>
-                    <Text style={{color: 'white'}}> {this.state.buttonText} </Text>
-                </View>
-            </TouchableHighlight>
-        </ScrollView>
-    );
-  }
+
+
+            </ScrollView>
+        );
+    }
 }
+
+// Layout of an x-axis together with a y-axis is a problem that stems from flexbox.
+// All react-native-svg-charts components support full flexbox and therefore all
+// layout problems should be approached with the mindset "how would I layout regular Views with flex in this way".
+// In order for us to align the axes correctly we must know the height of the x-axis or the width of the x-axis
+// and then displace the other axis with just as many pixels. Simple but manual.
+//example found at: https://github.com/JesperLekland/react-native-svg-charts-examples/blob/master/storybook/stories/both-axes.js
+
 
 const mapStateToProps = state => {
     return {
